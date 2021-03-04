aee9ff6b95bfbc9620a48f74b768e2712765274d
Clay OL <clay.oshiroleavitt@gmail.com>
2021-03-04 16:49:19 -0500

    functional stacked area chart, bar chart - need to link

--- dataclean.py
+++ dataclean.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
 generationData = pd.read_csv('data/generation_cleaned.csv')
-print(generationData.head())
 types = list(generationData)
 types.remove('year')
+generationData.to_csv('data/generation.csv', index=False)
 generationData['total'] = generationData[types].sum(axis=1)
-
-print(generationData.head())
 
 generationData.to_csv('data/generation_totals.csv', index=False)
