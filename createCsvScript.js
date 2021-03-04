a2298b90598f78442420bb6fbf00f94c1192c916
alancuriel <alannitro17@gmail.com>
2021-03-03 20:47:34 -0500

    Add script that creates csv from raw data

--- createCsvScript.js
+++ createCsvScript.js
@@ -4,16 +4,27 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
     path: 'englandTemp1772.csv',
     header: [
-        { id: 'trialNumber', title: 'TrailNumber' },
-        { id: 'expected', title: 'Expected' },
-        { id: 'error', title: 'Error' },
-        { id: 'reportedPercent', title: 'ReportedPercent' },
-        { id: 'vis', title: 'Visualization' },
+        { id: 'date', title: 'date' },
+        { id: 'value', title: 'value' }
     ]
 });
 
 
-const output = [];
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+let output = [];
 
 fs.readFile('./cetdl1772on.dat', 'utf8', (err, data) => {
     if (err) {
@@ -24,10 +35,27 @@ fs.readFile('./cetdl1772on.dat', 'utf8', (err, data) => {
     const lines = data.split('\n');
 
     for (let line of lines) {
-        console.log(line.split(' ').filter(i => i.trim().length > 0).length);
-    }
-})
+        const items = line.split(' ').filter(i => i.trim().length > 0)
 
-csvWriter
-    .writeRecords(output)
-    .then(() => console.log('The CSV file was written successfully'));
+        for (let i = 2; i < items.length; i++) {
+            if (items[i] !== '-999') {
+                var dt = new Date(items[0], i - 1, items[1]);
+                output.push({
+                    date: formatDate(dt),
+                    d: dt,
+                    value: items[i]
+                });
+            }
+        }
+    }
+
+    output.sort((dateA, dateB) => {
+        if (dateA.d < dateB.d) return -1;
+        if (dateA.d > dateB.d) return 1;
+        return 0;
+    });
+
+    csvWriter
+        .writeRecords(output)
+        .then(() => console.log('The CSV file was written successfully'));
+})
