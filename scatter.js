7f5954f37993aa7a070c4b3a417306fc3c7564aa
Danya Baron <dbaron@wpi.edu>
2021-03-05 09:58:43 -0500

    svg not generating in scatter

--- scatter.js
+++ scatter.js
@@ -1,10 +1,8 @@
-    // var margin = {top: 50, right: 100, bottom: 50, left: 100}
+    
     var width = 600
     var height = 700
 
     var margin = { top: 50, right: 30, bottom: 50, left: 100 }
-	// var height = 500 - margin.top - margin.bottom
-	// var width = 500 - margin.left - margin.right
 
 
     var svg = d3.select("#scatterPlot")
