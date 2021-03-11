d90a16c01dc9b0e04d537fc07d25b54a0bd6faef
DESKTOP-1HQKVIC\357 <neb.iron.ams@gmail.com>
2021-03-10 19:04:05 -0500

    done the painter algorithm for the keyboard. Looks good, now to add functionality.

--- VisConfig/VisMaker.js
+++ VisConfig/VisMaker.js
@@ -1,5 +1,5 @@
 
-const width = 500;
+const width = 600;
 const height = 800;
 
 const WHITE_KEY = 0;
@@ -8,15 +8,13 @@ const BLACK_KEY = 1;
 const WHITE_KEYS_PER_OCTAVE = 7;
 const BLACK_KEYS_PER_OCTAVE = 5;
 
-const layout = {topViz:{top: 10, bottom: 10, left: 10, right: 10},
+const layout = {topViz:{top: 10, bottom: 50, left: 10, right: 10},
     bottomVis:{top: 10, bottom: 10, left: 10, right: 10}, 
-    division: width};
+    division: height - width};
 
 const keyboard = {blackKeys:{heightReduce: 2, widthReduce: 2}};
 
-const svg = d3.select("#first_vis")
-                .attr("width", width)
-                .attr("height", height);
+let svg;
 
 function makeVis(majorThirds, equalTemperamentDifference)
 {        
@@ -25,6 +23,10 @@ function makeVis(majorThirds, equalTemperamentDifference)
     let noteTable = makeNoteTable(equalTemperamentDifference);
 
     // TODO: clear svg
+    svg = d3.select("#first_vis")
+        .attr("width", width)
+        .attr("height", height);
+
 
     // Makes the top keyboard vis
     makeKeyboardVis(noteTable);
@@ -39,36 +41,45 @@ function makeKeyboardVis(noteTable)
     // bottomVis:{top: 10, bottom: 10, left: 10, right: 10}, 
     // division: 500};
 
-    let whiteKeyWidth = ((layout.topViz.left + width) - layout.topViz.right) / noteTable.length;
-    let whiteKeyHeight = layout.division - layout.topViz.top - layout.topViz.bottom;
-    let blackKeyReducedProportional = whiteKeyWidth * (1 - 1/keyboard.blackKeys.widthReduce);
+    let whiteKeyWidth = ((width - layout.topViz.left) - layout.topViz.right) / ((WHITE_KEYS_PER_OCTAVE * gKeyboardOctaves) + 1);
+    let whiteKeyHeight = ((layout.division - layout.topViz.top) - layout.topViz.bottom);
+    let blackKeyAdjust = whiteKeyWidth/Math.pow(keyboard.blackKeys.widthReduce, 2);
 
-    // key x white coordinate, based on the placement
-    let keyxw = d3.scaleLinear()
-        .domain([0, WHITE_KEYS_PER_OCTAVE * gKeyboardOctaves])
-        .range([layout.topViz.left, (layout.topViz.left + width) - layout.topViz.right]);
-
-    // Key x black placement
-    let keyxb = d3.scaleLinear()
-        .domain([0, BLACK_KEYS_PER_OCTAVE * gKeyboardOctaves])
-        .range([layout.topViz.left + blackKeyReducedProportional,
-            ((layout.topViz.left + width) - layout.topViz.right) - blackKeyReducedProportional]);
-
-    let keyx = (k, keyIndex) =>
+    // Gets the key type from the key name: 0 is white keys, 1 is black keys
+    let keyType = (kName) => 
     {
-        let keyxs;
-        switch (k)
+        let k = 0;
+        // We assume that black keys include an accidental
+        if (kName.includes("b") || kName.includes("#"))
+        {
+            k = 1;
+        }
+        return k;
+    }
+
+
+    // The x position of each key is just calculated
+    //  doin it while drawing keys is just a pain
+    let keyxArray = [];
+    let whiteKeyx = layout.topViz.left;
+    for (let n = 0; n < noteTable.length; n++)
+    {
+        let kt = keyType(noteTable[n].key);
+        switch (kt)
         {
             case WHITE_KEY:
-                keyxs = keyxw(keyIndex);
+                keyxArray.push(whiteKeyx)
+                whiteKeyx += whiteKeyWidth;
                 break;
             case BLACK_KEY:
-                keyxs = keyxb(keyIndex);
+                keyxArray.push(whiteKeyx - blackKeyAdjust);
                 break;
             default:
                 break;
         }
     }
+    
+    keyx = (k) => { return ; };
     
     // Key y coordinate, always the same value
     let keyy = (k) => {return layout.topViz.top};
@@ -83,19 +94,67 @@ function makeKeyboardVis(noteTable)
         .domain([WHITE_KEY, BLACK_KEY])
         .range([whiteKeyHeight, whiteKeyHeight / keyboard.blackKeys.heightReduce]);
 
-    // Gets the key type from the key name: 0 is white keys, 1 is black keys
-    let keyType = (kName) => 
-    {
-        let k = 0;
-        // We assume that black keys include an accidental
-        if (kName.includes("b") || kName.includes("#"))
-        {
-            k = 1;
-        }
-        return k;
-    }
+    let keyColor = d3.scaleLinear()
+        .domain([WHITE_KEY, BLACK_KEY])
+        .range(["white", "black"]);
 
     
+
+    let drawKey = (kt, n) =>
+    {
+        svg.append("rect")
+            .attr('x', keyxArray[n]) 
+            .attr('y', keyy(kt))
+            .attr('width', keyWidth(kt))
+            .attr('height', keyHeight(kt))
+            .style("stroke", "black")
+            .style("stroke-width", 1)
+            .style("fill", keyColor(kt));
+    }
+
+    // Key drawing must be done white then black
+    // Draw white keys
+    for (let n = 0; n < noteTable.length; n++)
+    {
+        let kt = keyType(noteTable[n].key);
+        if (kt === WHITE_KEY)
+        {
+            drawKey(kt, n);
+        }
+    }
+
+    // Draw black keys
+    previousWhiteKeyx = layout.topViz.left; // used to judge where black keys should be
+    for (let n = 0; n < noteTable.length; n++)
+    {
+        let kt = keyType(noteTable[n].key);
+        if (kt === BLACK_KEY)
+        {
+            drawKey(kt, n);
+        }
+        else
+        {
+            //keyxw();
+        }
+    }
+    
+    
+
+    // svg.selectAll("rectangle")
+    //     .data(noteTable)
+    //     .enter()
+    //     .append("rect")
+    //         .attr('x', (k, n) => { return keyx(keyType(k.key), n); })
+    //         .attr('y', (k) => { return keyy(keyType(k.key)); })
+    //         .attr('width', (k) => { return keyWidth(keyType(k.key)); })
+    //         .attr('height', (k) => { return keyHeight(keyType(k.key)); })
+    //         .style("stroke", "black")
+    //         .style("stroke-width", 1)
+    //         .style("fill", "white");
+
+
+
+    // Draw names
 }
 
 function makeCircleVis(majorThirds)
